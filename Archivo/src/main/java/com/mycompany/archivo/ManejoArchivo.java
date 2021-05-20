9eda75d90116881a90c035dcdcaf0c263b882ec4
luisbaquiax <47260039+luisbaquiax@users.noreply.github.com>
2021-05-20 00:47:11 -0600

    pruebas con archivo

---
+++ Archivo/src/main/java/com/mycompany/archivo/ManejoArchivo.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.archivo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import static javax.swing.JFileChooser.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Luis
+ */
+public class ManejoArchivo {
+
+    private JFileChooser choser;
+    public static final String FILTRO = ".txt";
+
+    public String pathChoserSave() {
+
+        choser = new JFileChooser();
+        choser.addChoosableFileFilter(new FileNameExtensionFilter("Persona", "txt"));
+        choser.setAcceptAllFileFilterUsed(false);
+        int seleccionado = choser.showSaveDialog(null);
+        if (seleccionado == APPROVE_OPTION) {
+            return (choser.getSelectedFile().getAbsolutePath());
+        }
+        return null;
+    }
+
+    public String pathChoserOpen() {
+        choser = new JFileChooser();
+        choser.addChoosableFileFilter(new FileNameExtensionFilter("Persona", "txt"));
+        choser.setAcceptAllFileFilterUsed(false);
+        int seleccionado = choser.showOpenDialog(null);
+        if (seleccionado == APPROVE_OPTION) {
+            return (choser.getSelectedFile().getAbsolutePath());
+        }
+        return null;
+    }
+
+    public void escribirObjeto(String filepath, Object persona) {
+
+        try {
+
+            FileOutputStream fileOut = new FileOutputStream(filepath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+            objectOut.writeObject(persona);
+            objectOut.close();
+            System.out.println("Se ha escrito el objeto");
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Object leerObejtodelAarchivo(String filepath) {
+
+        try {
+
+            FileInputStream fileIn = new FileInputStream(filepath);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+
+            Object obj = objectIn.readObject();
+            System.out.println("Se ha leido el objeto");
+            objectIn.close();
+            return obj;
+
+        } catch (IOException | ClassNotFoundException ex) {
+            return null;
+        }
+    }
+
+    public ArrayList<Persona> personas(String path) {
+        try {
+            ObjectInputStream flujoSalida = new ObjectInputStream(new FileInputStream(path));
+            Object salida = flujoSalida.readObject();
+            flujoSalida.close();
+            return (ArrayList<Persona>) salida;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void escribirArchivodeTexto(String ruta, String contenido) {
+        try {
+            File file = new File(ruta);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter escribeArchivo = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(escribeArchivo);
+            bw.write(contenido);
+            bw.close();
+        } catch (IOException e) {
+        }
+    }
+}
---
+++ Archivo/src/main/java/com/mycompany/archivo/Persona.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.archivo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Luis
+ */
+public class Persona implements Serializable{
+
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidos
+     */
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    /**
+     * @param apellidos the apellidos to set
+     */
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + '}';
+    }
+
+}
